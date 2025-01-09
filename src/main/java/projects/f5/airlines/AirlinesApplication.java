07b998df0bb8d137ea59e42a80783577c6cebce6
tizzifona <tizzifona@gmail.com>
2025-01-09 18:50:04 +0100

    Added main structure of project

---
+++ src/main/java/projects/f5/airlines/AirlinesApplication.java
@@ -0,0 +1,13 @@
+package projects.f5.airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projects/f5/airlines/AirlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.f5.airlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
