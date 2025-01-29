28054b6d19355805a3b8fcfd4a7fff05dcdd21e9
tizzifona <tizzifona@gmail.com>
2025-01-29 23:43:37 +0100

    Fixes of security config

--- src/main/java/projects/f5/airlines/security/SecurityConfig.java
+++ src/main/java/projects/f5/airlines/security/SecurityConfig.java
@@ -41,20 +41,20 @@ public class SecurityConfig {
                         .requestMatchers("/api/login").permitAll()
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
 
+                        .requestMatchers("/api/users/{id}").hasRole("USER")
+                        .requestMatchers("/api/users/{id}/upload").hasRole("USER")
+                        .requestMatchers("/api/reservations/my-reservations").hasRole("USER")
+                        .requestMatchers("/api/reservations/create").hasRole("USER")
+                        .requestMatchers("/api/flights/search").hasRole("USER")
+
+                        .requestMatchers("/api/users/**").hasRole("ADMIN")
                         .requestMatchers("/api/airports/**").hasRole("ADMIN")
                         .requestMatchers("/api/flights/**").hasRole("ADMIN")
                         .requestMatchers("/api/reservations").hasRole("ADMIN")
                         .requestMatchers("/api/reservations/{id}/confirm").hasRole("ADMIN")
-                        .requestMatchers("/api/users/{id}/reservations").hasRole("ADMIN")
-
-                        .requestMatchers("/api/reservations/my-reservations").hasRole("USER")
-                        .requestMatchers("/api/reservations/create").hasRole("USER")
-
-                        .requestMatchers("/api/flights/search").hasRole("USER")
-                        .requestMatchers("/api/users/{id}").hasRole("USER")
-                        .requestMatchers("/api/users/{id}/upload").hasRole("USER")
 
                         .anyRequest().authenticated())
+
                 .userDetailsService(jpaUserDetailsService)
                 .httpBasic(withDefaults())
                 .sessionManagement(session -> session
