3b9091cba2108cc83e7cfad3bcb2a0386fb00368
tizzifona <tizzifona@gmail.com>
2025-01-31 09:54:25 +0100

    Added fixes for permissions to confirm reservations

--- src/main/java/projects/f5/airlines/security/SecurityConfig.java
+++ src/main/java/projects/f5/airlines/security/SecurityConfig.java
@@ -45,12 +45,14 @@ public class SecurityConfig {
                         .requestMatchers("/api/users/{id}/upload").hasRole("USER")
                         .requestMatchers("/api/reservations/my-reservations").hasRole("USER")
                         .requestMatchers("/api/reservations/create").hasRole("USER")
+                        .requestMatchers("/api/reservations/{id}/confirm").hasRole("USER")
                         .requestMatchers("/api/flights/search").hasRole("USER")
 
                         .requestMatchers("/api/users").hasRole("ADMIN")
                         .requestMatchers("/api/airports/**").hasRole("ADMIN")
                         .requestMatchers("/api/flights/**").hasRole("ADMIN")
                         .requestMatchers("/api/reservations").hasRole("ADMIN")
+                        .requestMatchers("/api/reservations/{id}").hasRole("ADMIN")
 
                         .anyRequest().authenticated())
 
